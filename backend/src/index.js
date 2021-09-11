336f36a3d2d5efa1d838f70bab3c62c329eeeaf2
Matheus Ferreira <matheusfs97@gmail.com>
2021-09-11 19:05:39 -0300

    hotfix

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect(process.env?.MONGO_URL ?? 'mongodb://localhost:27017/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -16,4 +16,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env?.PORT ?? 3333);
+app.listen(process.env.PORT);
