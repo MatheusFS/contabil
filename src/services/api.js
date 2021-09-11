87fa762b44d14536cd484f7ee2e5272377047ffd
Matheus Ferreira <matheusfs97@gmail.com>
2021-09-11 19:05:39 -0300

    hotfix

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 require('dotenv/config');
 
 const api = axios.create({
-    baseURL: (process.env?.API_URL ?? 'http://localhost:3333')
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
